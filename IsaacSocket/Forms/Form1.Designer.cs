4b198a7dbad82783ca0fbe8c88254c4873afe811
LanbingIce <895446745@qq.com>
2023-09-14 16:40:45 +0800

    修改了托盘图标的提示文字为"IsaacSocket"

--- IsaacSocket/Forms/Form1.Designer.cs
+++ IsaacSocket/Forms/Form1.Designer.cs
@@ -55,7 +55,7 @@ partial class Form1
         // 
         notifyIcon1.ContextMenuStrip = contextMenuStrip1;
         notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
-        notifyIcon1.Text = "notifyIcon1";
+        notifyIcon1.Text = "IsaacSocket";
         notifyIcon1.Visible = true;
         notifyIcon1.DoubleClick += notifyIcon1_DoubleClick;
         // 
