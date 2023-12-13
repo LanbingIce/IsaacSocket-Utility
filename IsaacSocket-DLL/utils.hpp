397b3c7c5ac0f1f3354af6ea5043467ec3b78af6
LanbingIce <895446745@qq.com>
2023-12-13 23:18:19 +0800

    添加常用工具函数的头文件utils.hpp

---
+++ IsaacSocket-DLL/utils.hpp
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "pch.h"
+
+namespace utils {
+	static std::string ToHexString(uint32_t num)
+	{
+		std::ostringstream oss;
+		oss << std::hex << std::setiosflags(std::ios::uppercase) << num;
+		return oss.str();
+	}
+
+	static void Utf8Cprintf(const char* format, ...) {
+		va_list v;
+		va_start(v, format);
+		size_t size = vsnprintf(nullptr, 0, format, v) + 1;
+		char* u8Str = new char[size];
+		vsnprintf(u8Str, size, format, v);
+		va_end(v);
+		size = MultiByteToWideChar(CP_UTF8, 0, u8Str, -1, nullptr, 0);
+		wchar_t* u16Str = new wchar_t[size];
+		MultiByteToWideChar(CP_UTF8, 0, u8Str, -1, u16Str, size);
+		_cwprintf(u16Str);
+		delete[] u16Str;
+		delete[] u8Str;
+	}
+
+	template <typename T>
+	void cw(const T& value) {
+		if (GetConsoleWindow())
+		{
+			ostringstream oss;
+			oss << value << '\n';
+			Utf8Cprintf(oss.str().c_str());
+		}
+	}
+}
