6e14dcb305d74768a6591dd8e9f8577271d86b0f
LanbingIce <895446745@qq.com>
2023-12-14 08:04:09 +0800

    将注入代码部分分离到inject.hpp，并完善

--- IsaacSocket-DLL/dllmain.cpp
+++ IsaacSocket-DLL/dllmain.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <vector>
 #include "isaac.hpp"
+#include "inject.hpp"
 
 using std::endl;
 using std::hex;
@@ -24,16 +25,11 @@ struct Config
 	int IsForcePaused;
 	int needPrint;
 };
-// 对注入的函数进行声明
-static void __fastcall Render_inj();
-static void __fastcall GameUpdate_inj(isaac::Game* gamePtr);
-static void __fastcall ConsoleOutput_inj(isaac::Console& console, void*, const string& text, int color, int type_guess);
-static void __fastcall ExecuteCommand_inj(isaac::Console& console, void*, const string& text, int, int);
-static void __fastcall AdditionalUpdate_inj();
-static bool __fastcall IsPaused_inj(isaac::Game* gamePtr);
+
 // IsaacSocket类，实现具体功能
 struct IsaacSocket
 {
+	static inline HANDLE hProcess;
 	static inline isaac::IsaacImage* isaac;
 	static inline Config* config;
 	// int转16进制文本
@@ -43,104 +39,53 @@ struct IsaacSocket
 		os << hex << setiosflags(ios::uppercase) << num;
 		return os.str();
 	}
-	static void SaveText(string& text) {
-		ofstream outfile;
-		outfile.open("%temp%/IsaacSocket.txt", ios::app);
-		outfile << text << std::endl;
-		outfile.close();
-	}
+
 	// 重新载入lua环境
-	static void ResetLua()
+	static void ReloadLuaWithoutDeleteRoom()
 	{
-		void* thisPtr = (char*)(isaac->luaVM) + 0x00029FD8;
-		int d_0x7FD674 = *(int*)((unsigned)isaac + 0x7FD674);
-		void (*f_0x55E330)(void*, int) = (void (*)(void*, int))((unsigned)isaac + 0x55E330);
-		// 输出日志
-		f_0x55E330(0, (int)isaac + 0x74F6F4);
-		//"Lua is resetting!"
-		void(__fastcall * f_0x40AE00)(int) = (void(__fastcall*)(int))((unsigned)isaac + 0x40AE00);
-		// 卸载lua环境
-		f_0x40AE00(d_0x7FD674);
-		void(__fastcall * f_0x3FCB00)(int, int, int) = (void(__fastcall*)(int, int, int))((unsigned)isaac + 0x3FCB00);
-		// 加载lua环境
-		f_0x3FCB00(d_0x7FD674, NULL, *(char*)(d_0x7FD674 + 0x1C));
-		void(__fastcall * f_0x4702F0)(void*) = (void(__fastcall*)(void*))((unsigned)isaac + 0x4702F0);
-		// 清除mod列表
-		f_0x4702F0(thisPtr);
-		void(__fastcall * f_0x470B40)(void*) = (void(__fastcall*)(void*))((unsigned)isaac + 0x470B40);
-		// 创建mod列表
-		f_0x470B40(thisPtr);
-		void(__fastcall * f_0x46F2B0)(void*) = (void(__fastcall*)(void*))((unsigned)isaac + 0x46F2B0);
-		// 重新加载着色器
-		f_0x46F2B0(thisPtr);
-		void (*f_0x4AF200)() = (void (*)())((unsigned)isaac + 0x4AF200);
-		// 可能是重新加载精灵
+#define _(address,...) auto f_##address = (void(__fastcall*)(__VA_ARGS__))((char*)isaac + address)
+		inject::LogPrintf(0, "Lua is resetting!\n");									// 输出日志
+		_(0x40AE00, isaac::LuaVM*);													// 卸载lua环境
+		f_0x40AE00(isaac->luaVM);
+		_(0x3FCB00, isaac::LuaVM*, void*, int);										// 加载lua环境
+		f_0x3FCB00(isaac->luaVM, NULL, isaac->luaVM->luaDebug);
+		_(0x4702F0, isaac::ModManager&);											// 清除mod列表
+		f_0x4702F0(isaac->fileManager->modManager);
+		_(0x470B40, isaac::ModManager&);											// 创建mod列表
+		f_0x470B40(isaac->fileManager->modManager);
+		_(0x46F2B0, isaac::ModManager&);											// 重新加载着色器
+		f_0x46F2B0(isaac->fileManager->modManager);
+		_(0x4AF200);																// 重新加载精灵和字体
 		f_0x4AF200();
-		void(__fastcall * f_0x46DAE0)(void*) = (void(__fastcall*)(void*))((unsigned)isaac + 0x46DAE0);
-		// 加载lua环境
-		f_0x46DAE0(thisPtr);
-		int d_0x7FD688 = *(int*)((unsigned)isaac + 0x7FD688);
-		if (d_0x7FD688)
-		{
-			if (isaac->game)
-			{
-				void(__fastcall * f_0x517F20)(isaac::PauseMenu&) = (void(__fastcall*)(isaac::PauseMenu&))((unsigned)isaac + 0x517F20);
-				// 不懂，可能是某种初始化
-				f_0x517F20(isaac->game->pauseMenu);
-			}
-			void (*f_0x92A0)() = (void (*)())((unsigned)isaac + 0x92A0);
-			// 可能是清除什么缓存
-			f_0x92A0();
-			void (*f_0x4AFA70)() = (void (*)())((unsigned)isaac + 0x4AFA70);
-			// 可能是重新加载精灵
-			f_0x4AFA70();
-			void(__fastcall * f_0x4A3F30)(int) = (void(__fastcall*)(int))((unsigned)isaac + 0x4A3F30);
-			// 不懂
-			f_0x4A3F30(d_0x7FD688 + 0x101C00);
-			void(__fastcall * f_0x4AB4E0)(int) = (void(__fastcall*)(int))((unsigned)isaac + 0x4AB4E0);
-			// 不懂
-			f_0x4AB4E0(d_0x7FD688 + 0x102E80);
-		}
+		_(0x46DAE0, isaac::ModManager&);											// 重新加载xml
+		f_0x46DAE0(isaac->fileManager->modManager);
+#undef _
 	}
-	// 获取“是否强制暂停”的配置，方便注入的汇编函数调用
-	static bool IsForcePaused()
-	{
-		return config->IsForcePaused;
-	}
-	// 进行一次“获取游戏是否暂停”
-	static bool IsPaused()
-	{
-		return IsPaused_inj(isaac->game);
-	}
+
 	// 进行一次“额外更新”，即人物更新之类的每秒额外30次更新
-	static void AdditionalUpdate()
+	static void SpecialUpdate()
 	{
-		AdditionalUpdate_inj();
+		inject::SpecialUpdate();
 	}
 	// 进行一次“游戏更新”
 	static void GameUpdate()
 	{
-		GameUpdate_inj(isaac->game);
+		inject::GameUpdate(isaac->game);
 	}
 	// 进行一次“渲染”
 	static void Render()
 	{
-		Render_inj();
+		inject::Render();
 	}
 	// 进行一次“控制台输出”
 	static void ConsoleOutput(const string& text, unsigned int color = 0xFFD3D3D3)
 	{
-		ConsoleOutput_inj(isaac->game->console, NULL, text, color, 0x96);
-	}
-	// 进行一次“控制台输出”，并追加换行符
-	static void ConsoleOutputLine(const string& text = "", unsigned int color = 0xFFD3D3D3)
-	{
-		ConsoleOutput_inj(isaac->game->console, NULL, text + "\n", color, 0x96);
+		inject::ConsoleOutput(isaac->game->console, NULL, text, color, 0x96);
 	}
 	// 进行一次“执行控制台指令”
 	static void ExecuteCommand(const string& commandText)
 	{
-		ExecuteCommand_inj(isaac->game->console, NULL, commandText, 0, 0);
+		inject::ExecuteCommand(isaac->game->console, NULL, commandText, 0, 0);
 	}
 	// 渲染回调，时机在渲染函数的起始位置，只要游戏进程存在就一直触发
 	static void OnRender()
@@ -148,155 +93,50 @@ struct IsaacSocket
 		if (config->needReload == 1)
 		{
 			config->needReload = 0;
-			ResetLua();
+			IsaacSocket::ReloadLuaWithoutDeleteRoom();
 		}
 	}
 	// 执行控制台指令回调，时机在执行控制台指令函数的起始位置
-	static void OnExecuteCommand(string& str)
+	static void OnExecuteCommand(string& str, int unknow, int unknow_point_guess)
 	{
-
+		if (str == "lualua")
+		{
+			config->needReload = 1;
+		}
+		if (str == "ac")
+		{
+			AllocConsole();
+		}
+		if (str == "fc")
+		{
+			FreeConsole();
+		}
 	}
+
 	// 额外更新回调，时机在额外更新函数的起始位置
-	static void OnAdditionalUpdate()
+	static void OnSpecialUpdate()
 	{
 
 	}
+
 	// 游戏更新回调，时机在游戏更新函数的起始位置
 	static void OnGameUpdate()
 	{
 
 	}
-	// 控制台输出回调，时机在控制台输出函数的起始位置
-	static void OnConsoleOutput(string& str, int color)
+
+	// MT随机回调
+	static uint32_t __fastcall OnMTRandom(uint32_t num)
 	{
-		SaveText(str);
+		return num;
+	}
+
+	// 控制台输出回调，时机在控制台输出函数的起始位置
+	static void OnConsoleOutput(string& str, int color, int type)
+	{
+		//utils::cw(str + " " + to_string(type));
 	}
 };
-// 以下为注入的函数
-// 渲染函数
-__declspec(naked) void __fastcall Render_inj()
-{
-	__asm {
-		call IsaacSocket::OnRender
-		push ebp
-		mov ebp, esp
-		and esp, -0x08
-		mov eax, IsaacSocket::isaac
-		add eax, 0x4B0606
-		jmp eax
-	}
-}
-// 游戏更新函数
-__declspec(naked) void __fastcall GameUpdate_inj(isaac::Game* gamePtr)
-{
-	__asm {
-		push ecx
-		call IsaacSocket::OnGameUpdate
-		call IsaacSocket::IsForcePaused
-		cmp al, 0x0
-		pop ecx
-		jne flag
-		push ebp
-		mov ebp, esp
-		and esp, -0x10
-		mov eax, IsaacSocket::isaac
-		add eax, 0x2CDCF6
-		jmp eax
-		flag :
-		ret
-	}
-}
-// 额外更新函数
-__declspec(naked) void __fastcall AdditionalUpdate_inj()
-{
-	__asm {
-		call IsaacSocket::OnAdditionalUpdate
-		call IsaacSocket::IsForcePaused
-		cmp al, 0x0
-		jne flag
-		push ebp
-		mov ebp, esp
-		sub esp, 0x0C
-		mov eax, IsaacSocket::isaac
-		add eax, 0x2D0406
-		jmp eax
-		flag :
-		ret
-	}
-}
-// 执行控制台指令函数
-__declspec(naked) void __fastcall ExecuteCommand_inj(isaac::Console& console, void*, const string& text, int, int)
-{
-	__asm {
-		push ecx
-		push[esp + 0x8]
-		call IsaacSocket::OnExecuteCommand
-		add esp, 0x4
-		pop ecx
-		push ebp
-		mov ebp, esp
-		push - 0x01
-		mov eax, IsaacSocket::isaac
-		add eax, 0x2655C5
-		jmp eax
-	}
-}
-// 控制台输出函数
-__declspec(naked) void __fastcall ConsoleOutput_inj(isaac::Console& console, void*, const string& text, int color, int type_guess)
-{
-	__asm {
-		push ecx
-		push[esp + 0xC]
-		push[esp + 0xC]
-		call IsaacSocket::OnConsoleOutput
-		add esp, 8
-		pop ecx
-		push ebp
-		mov ebp, esp
-		push - 0x1
-		mov eax, IsaacSocket::isaac
-		add eax, 0x26AEC5
-		jmp eax
-	}
-}
-// 游戏是否暂停函数
-__declspec(naked) bool __fastcall IsPaused_inj(isaac::Game* gamePtr)
-{
-	__asm {
-		push ecx
-		call IsaacSocket::IsForcePaused
-		cmp al, 00
-		pop ecx
-		jne flag
-		mov eax, IsaacSocket::isaac
-		add eax, 0x2D0387
-		cmp dword ptr[ecx + 0x1BB80], 0
-		jmp eax
-		flag :
-		mov eax, IsaacSocket::isaac
-			add eax, 0x2D03F8
-			jmp eax
-	}
-}
-// 以上为注入的函数
-
-//注入函数
-static void Inject(int offset, void* fuctionAddress, int paddingSize)
-{
-	HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId());
-	int injectAddress = (int)IsaacSocket::isaac + offset;
-	unsigned char c = 0xE9;
-	WriteProcessMemory(processHandle, (void*)(injectAddress), &c, sizeof(c), nullptr);
-	int value = (int)fuctionAddress - (injectAddress + 5);
-	WriteProcessMemory(processHandle, (void*)(injectAddress + 1), &value, sizeof(value), nullptr);
-	c = 0x90;
-	if (paddingSize > 0)
-	{
-		int pad = 0x90909090;
-		WriteProcessMemory(processHandle, (void*)(injectAddress + 5), &pad, paddingSize, nullptr);
-	}
-	CloseHandle(processHandle);
-}
 
 // 初始化，共享内存和注入
 static void Init()
@@ -304,20 +144,19 @@ static void Init()
 	HANDLE hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 1024, L"IsaacSocketSharedMemory");
 	if (hMapFile)
 	{
+		IsaacSocket::hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, GetCurrentProcessId());
 		IsaacSocket::config = (Config*)MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, 1024);
 		IsaacSocket::isaac = (isaac::IsaacImage*)GetModuleHandle(NULL);
-		// 渲染
-		Inject(0x4B0600, &Render_inj, 1);
-		// 执行控制台指令
-		Inject(0x2655C0, &ExecuteCommand_inj, 0);
-		// 控制台输出
-		Inject(0x26AEC0, &ConsoleOutput_inj, 0);
-		// 是否暂停
-		Inject(0x2D0380, &IsPaused_inj, 2);
-		// 游戏更新
-		Inject(0x2CDCF0, &GameUpdate_inj, 1);
-		// 附加更新
-		Inject(0x2D0400, &AdditionalUpdate_inj, 1);
+		inject::Callbacks callbacks =
+		{
+			IsaacSocket::OnRender,
+			IsaacSocket::OnGameUpdate,
+			IsaacSocket::OnSpecialUpdate,
+			IsaacSocket::OnExecuteCommand,
+			IsaacSocket::OnConsoleOutput,
+			IsaacSocket::OnMTRandom,
+		};
+		inject::Init(IsaacSocket::hProcess, IsaacSocket::isaac, callbacks);
 	}
 }
 BOOL APIENTRY DllMain(HMODULE hModule,
---
+++ IsaacSocket-DLL/inject.hpp
@@ -0,0 +1,178 @@
+﻿#pragma once
+#include "pch.h"
+#include "utils.hpp"
+
+namespace inject {
+	struct Callbacks
+	{
+		LPCVOID OnRender;
+		LPCVOID OnGameUpdate;
+		LPCVOID OnSpecialUpdate;
+		LPCVOID OnExecuteCommand;
+		LPCVOID OnConsoleOutput;
+		uint32_t(__fastcall* OnMTRandom)(uint32_t);
+	};
+
+	static const char* logPreFix = "[LOG(%d)] ";
+	static HANDLE hProcess;
+	static isaac::IsaacImage* isaac;
+	static Callbacks callbacks;
+	static int tmpRetIP;
+	static int tmpLogLevel;
+
+	//输出日志函数
+	__declspec(naked) void LogPrintf(int level, const char* format, ...) {
+		__asm {
+			call GetConsoleWindow
+			test eax, eax
+			je flag
+			push[esp + 0x04]
+			push logPreFix
+			call _cprintf
+			add esp, 0x08
+			pop tmpRetIP
+			pop tmpLogLevel
+			call utils::Utf8Cprintf
+			push tmpLogLevel
+			push tmpRetIP
+			flag : push ebp
+			mov ebp, esp
+			and esp, -0x08
+			mov eax, isaac
+			add eax, 0x55E336
+			jmp eax
+		}
+	}
+
+	// 渲染函数
+	__declspec(naked) void Render()
+	{
+		__asm {
+			call callbacks.OnRender
+			push ebp
+			mov ebp, esp
+			and esp, -0x08
+			mov eax, isaac
+			add eax, 0x4B0606
+			jmp eax
+		}
+	}
+
+	// 游戏更新函数，每秒30次更新
+	__declspec(naked) void __fastcall GameUpdate(isaac::Game* gamePtr)
+	{
+		__asm {
+			push ecx
+			call callbacks.OnGameUpdate
+			pop ecx
+			push ebp
+			mov ebp, esp
+			and esp, -0x10
+			mov eax, isaac
+			add eax, 0x2CDCF6
+			jmp eax
+		}
+	}
+
+	// 特殊更新函数，用来更新某些需要每秒60次更新的东西，游戏更新函数每秒只触发30次，另外30次在这个函数里处理
+	__declspec(naked) void SpecialUpdate()
+	{
+		__asm {
+			call callbacks.OnSpecialUpdate
+			push ebp
+			mov ebp, esp
+			sub esp, 0x0C
+			mov eax, isaac
+			add eax, 0x2D0406
+			jmp eax
+		}
+	}
+
+	// 执行控制台指令函数,最后两个参数总是0,如果最后一个参数不是0，游戏就会崩溃
+	__declspec(naked) void __fastcall ExecuteCommand(isaac::Console& console, LPCVOID _, const string& text, int unknow, int unknow_point_guess)
+	{
+		__asm {
+			push ecx
+			push[esp + 0x10]
+			push[esp + 0x10]
+			push[esp + 0x10]
+			call callbacks.OnExecuteCommand
+			add esp, 0x0C
+			pop ecx
+			push ebp
+			mov ebp, esp
+			push - 0x01
+			mov eax, isaac
+			add eax, 0x2655C5
+			jmp eax
+		}
+	}
+
+	// 控制台输出函数,最后一个参数总是0x96，只有当控制台顶部的"Repentance Console"这行字输出时，这个参数是0
+	__declspec(naked) void __fastcall ConsoleOutput(isaac::Console& console, LPCVOID _, const string& text, uint32_t color, uint32_t type_guess)
+	{
+		__asm {
+			push ecx
+			push[esp + 0x10]
+			push[esp + 0x10]
+			push[esp + 0x10]
+			call callbacks.OnConsoleOutput
+			add esp, 0x0C
+			pop ecx
+			push ebp
+			mov ebp, esp
+			push - 0x01
+			mov eax, isaac
+			add eax, 0x26AEC5
+			jmp eax
+		}
+	}
+
+	// MT19937随机数生成,这个函数的注入点在尾部，因此不会影响正常的随机序列，且可以获取并覆盖原函数的返回值
+	// 这个函数对原代码做了优化,节省了部分代码，回调函数的调用约定为fastcall
+	__declspec(naked) void MTRandom()
+	{
+		__asm {
+			shr eax, 0x12
+			xor ecx, eax
+			call callbacks.OnMTRandom
+			ret
+		}
+	}
+
+	//注入代码
+	static void InjectCode(size_t offset, LPCVOID fuctionAddress, size_t paddingSize)
+	{
+		char* injectAddress = (char*)isaac + offset;
+		//操作码长度为1，操作数长度为4，两者加起来是5
+		uint8_t opcode = 0xE9;//jmp
+		WriteProcessMemory(hProcess, injectAddress, &opcode, 1, nullptr);
+		int32_t operand = (char*)fuctionAddress - injectAddress - 5;
+		WriteProcessMemory(hProcess, injectAddress + 1, &operand, 4, nullptr);
+		for (size_t i = 0; i < paddingSize; i++)
+		{
+			opcode = 0x90;//nop
+			WriteProcessMemory(hProcess, injectAddress + 5 + i, &opcode, 1, nullptr);
+		}
+	}
+
+	void Init(HANDLE hProcess, isaac::IsaacImage* isaac, Callbacks callbacks) {
+		inject::hProcess = hProcess;
+		inject::isaac = isaac;
+		inject::callbacks = callbacks;
+		// 渲染
+		InjectCode(0x4B0600, Render, 1);
+		// 执行控制台指令
+		InjectCode(0x2655C0, ExecuteCommand, 0);
+		// 控制台输出
+		InjectCode(0x26AEC0, ConsoleOutput, 0);
+		// 游戏更新
+		InjectCode(0x2CDCF0, GameUpdate, 1);
+		// 特殊更新
+		InjectCode(0x2D0400, SpecialUpdate, 1);
+		// 日志输出
+		InjectCode(0x55E330, LogPrintf, 1);
+		// MT19937随机数生成
+		InjectCode(0x2C3EA8, MTRandom, 1);
+	}
+}
