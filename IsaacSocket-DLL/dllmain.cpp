434a96e2604f49dac2d5da32d1b94681c7b10f00
LanbingIce <895446745@qq.com>
2023-12-14 23:49:27 +0800

    将执行以撒原函数部分分离到function.hpp，并完善

--- IsaacSocket-DLL/dllmain.cpp
+++ IsaacSocket-DLL/dllmain.cpp
@@ -3,6 +3,7 @@
 #include "isaac.hpp"
 #include "inject.hpp"
 #include "state.hpp"
+#include "function.hpp"
 
 // IsaacSocket类，实现具体功能
 struct IsaacSocket
@@ -11,65 +12,23 @@ struct IsaacSocket
 	static inline isaac::IsaacImage* isaac;
 	static inline state::StateData* stateData;
 
-	// 重新载入lua环境
-	static void ReloadLuaWithoutDeleteRoom()
-	{
-#define _(address,...) auto f_##address = (void(__fastcall*)(__VA_ARGS__))((char*)isaac + address)
-		inject::LogPrintf(0, "Lua is resetting!\n");									// 输出日志
-		_(0x40AE00, isaac::LuaVM*);													// 卸载lua环境
-		f_0x40AE00(isaac->luaVM);
-		_(0x3FCB00, isaac::LuaVM*, void*, int);										// 加载lua环境
-		f_0x3FCB00(isaac->luaVM, NULL, isaac->luaVM->luaDebug);
-		_(0x4702F0, isaac::ModManager&);											// 清除mod列表
-		f_0x4702F0(isaac->fileManager->modManager);
-		_(0x470B40, isaac::ModManager&);											// 创建mod列表
-		f_0x470B40(isaac->fileManager->modManager);
-		_(0x46F2B0, isaac::ModManager&);											// 重新加载着色器
-		f_0x46F2B0(isaac->fileManager->modManager);
-		_(0x4AF200);																// 重新加载精灵和字体
-		f_0x4AF200();
-		_(0x46DAE0, isaac::ModManager&);											// 重新加载xml
-		f_0x46DAE0(isaac->fileManager->modManager);
-#undef _
-	}
-
-	// 进行一次“额外更新”，即人物更新之类的每秒额外30次更新
-	static void SpecialUpdate()
-	{
-		inject::SpecialUpdate();
-	}
-	// 进行一次“游戏更新”
-	static void GameUpdate()
-	{
-		inject::GameUpdate(isaac->game);
-	}
-	// 进行一次“渲染”
-	static void Render()
-	{
-		inject::Render();
-	}
-	// 进行一次“控制台输出”
-	static void ConsoleOutput(const string& text, unsigned int color = 0xFFD3D3D3)
-	{
-		inject::ConsoleOutput(isaac->game->console, NULL, text, color, 0x96);
-	}
-	// 进行一次“执行控制台指令”
-	static void ExecuteCommand(const string& commandText)
-	{
-		inject::ExecuteCommand(isaac->game->console, NULL, commandText, 0, 0);
-	}
 	// 渲染回调，时机在渲染函数的起始位置，只要游戏进程存在就一直触发
 	static void OnRender()
 	{
 		if (stateData->needReload)
 		{
 			stateData->needReload = false;
-			IsaacSocket::ReloadLuaWithoutDeleteRoom();
+			function::ReloadLuaWithoutDeleteRoom();
 		}
 	}
 	// 执行控制台指令回调，时机在执行控制台指令函数的起始位置
 	static void OnExecuteCommand(string& str, int unknow, int unknow_point_guess)
 	{
+		if (str == "test")
+		{
+			//function::ExecuteCommand("g k1");
+			//function::ConsoleOutput("test");
+		}
 		if (str == "lualua")
 		{
 			stateData->needReload = true;
@@ -128,6 +87,7 @@ static void Init()
 			IsaacSocket::OnMTRandom,
 		};
 		inject::Init(IsaacSocket::hProcess, IsaacSocket::isaac, callbacks);
+		function::Init(IsaacSocket::isaac);
 	}
 }
 BOOL APIENTRY DllMain(HMODULE hModule,
---
+++ IsaacSocket-DLL/function.hpp
@@ -0,0 +1,74 @@
+﻿#pragma once
+
+#include "pch.h"
+#include "isaac.hpp"
+#include "inject.hpp"
+
+namespace function {
+
+	isaac::IsaacImage* isaac;
+
+	static void Init(isaac::IsaacImage* isaac) {
+		function::isaac = isaac;
+	}
+
+	// 进行一次 “额外更新”，即人物更新之类的每秒额外30次更新
+	static void SpecialUpdate()
+	{
+		inject::SpecialUpdate();
+	}
+
+	// 进行一次 “游戏更新”
+	static void GameUpdate()
+	{
+		inject::GameUpdate(isaac->game);
+	}
+
+	// 进行一次“渲染”
+	static void Render()
+	{
+		inject::Render();
+	}
+
+	// 进行一次 “控制台输出”
+	static void ConsoleOutput(const string& text, uint32_t color = 0xFFD3D3D3)
+	{
+		inject::ConsoleOutput(isaac->game->console, NULL, text, color, 0x96);
+	}
+
+	// 进行一次 “执行控制台指令”
+	static void ExecuteCommand(const string& commandText)
+	{
+		inject::ExecuteCommand(isaac->game->console, NULL, commandText, 0, 0);
+	}
+
+	// 重新载入lua环境
+	static void ReloadLuaWithoutDeleteRoom()
+	{
+#define _(offset,...) auto f_##offset = (void(__fastcall*)(__VA_ARGS__))((char*)isaac + offset)
+		// 输出日志
+		inject::LogPrintf(0, "Lua is resetting!\n");
+		// 卸载lua环境
+		_(0x40AE00, isaac::LuaVM*);
+		f_0x40AE00(isaac->luaVM);
+		// 加载lua环境
+		_(0x3FCB00, isaac::LuaVM*, LPCVOID, bool);
+		f_0x3FCB00(isaac->luaVM, NULL, isaac->luaVM->luaDebug);
+		// 清除mod列表
+		_(0x4702F0, isaac::ModManager&);
+		f_0x4702F0(isaac->fileManager->modManager);
+		// 创建mod列表
+		_(0x470B40, isaac::ModManager&);
+		f_0x470B40(isaac->fileManager->modManager);
+		// 重新加载着色器
+		_(0x46F2B0, isaac::ModManager&);
+		f_0x46F2B0(isaac->fileManager->modManager);
+		// 重新加载精灵和字体
+		_(0x4AF200);
+		f_0x4AF200();
+		// 重新加载xml
+		_(0x46DAE0, isaac::ModManager&);
+		f_0x46DAE0(isaac->fileManager->modManager);
+#undef _
+	}
+}
