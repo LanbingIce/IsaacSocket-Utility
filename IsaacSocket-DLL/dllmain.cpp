dedabec1cf948ceb4320d5a38e1842d4ccd187af
LanbingIce <895446745@qq.com>
2023-12-15 15:57:37 +0800

    将dll初始化逻辑从dllmain.cpp分离到isaac_socket.hpp，并完善

--- IsaacSocket-DLL/dllmain.cpp
+++ IsaacSocket-DLL/dllmain.cpp
@@ -1,37 +1,7 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "pch.h"
-#include "isaac.hpp"
-#include "inject.hpp"
-#include "state.hpp"
-#include "function.hpp"
-#include "lua.hpp"
-#include "isaac_api.hpp"
-#include "callback.hpp"
+#include "isaac_socket.hpp"
 
-// IsaacSocket类，实现具体功能
-struct IsaacSocket
-{
-	static inline HANDLE hProcess;
-	static inline isaac::IsaacImage* isaac;
-	static inline state::StateData* stateData;
-	static inline lua::Lua* lua;
-};
-
-// 初始化，共享内存和注入
-static void Init()
-{
-	HANDLE hMapFile = CreateFileMappingA(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(state::StateData), "IsaacSocketSharedMemory");
-	if (hMapFile)
-	{
-		IsaacSocket::hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, GetCurrentProcessId());
-		IsaacSocket::stateData = (state::StateData*)MapViewOfFile(hMapFile, FILE_MAP_WRITE, 0, 0, 0);
-		IsaacSocket::isaac = (isaac::IsaacImage*)GetModuleHandleA(NULL);
-		inject::Init(IsaacSocket::hProcess, IsaacSocket::isaac, callback::GetCallbacks());
-		function::Init(IsaacSocket::isaac);
-		IsaacSocket::lua = new lua::Lua{ GetModuleHandleA("Lua5.3.3r.dll") };
-		callback::Init(IsaacSocket::stateData, IsaacSocket::isaac, IsaacSocket::hProcess, IsaacSocket::lua);
-	}
-}
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD ul_reason_for_call,
 	LPVOID lpReserved)
@@ -39,7 +9,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		Init();
+		isaac_socket::Init();
 		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
---
+++ IsaacSocket-DLL/isaac_socket.hpp
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "pch.h"
+#include "lua.hpp"
+#include "state.hpp"
+#include "function.hpp"
+#include "inject.hpp"
+#include "callback.hpp"
+
+namespace isaac_socket{
+
+	static HANDLE hProcess;
+	static isaac::IsaacImage* isaac;
+	static state::StateData* stateData;
+	static lua::Lua* lua;
+
+	// 初始化，共享内存和注入
+	static void Init(){
+		HANDLE hMapFile = CreateFileMappingA(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(state::StateData), "IsaacSocketSharedMemory");
+		if (hMapFile)
+		{
+			hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, GetCurrentProcessId());
+			lua = new lua::Lua{ GetModuleHandleA("Lua5.3.3r.dll") };
+			stateData = (state::StateData*)MapViewOfFile(hMapFile, FILE_MAP_WRITE, 0, 0, 0);
+			isaac = (isaac::IsaacImage*)GetModuleHandleA(NULL);
+			function::Init(isaac);
+			callback::Init(stateData, isaac, hProcess, lua);
+			inject::Init(hProcess, isaac, callback::GetCallbacks());
+		}
+	}
+}
