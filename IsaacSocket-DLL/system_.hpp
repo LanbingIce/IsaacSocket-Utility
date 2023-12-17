e85e58de1085103eb46f7a07a56a5b5cb2bf7978
LanbingIce <895446745@qq.com>
2023-12-17 12:21:02 +0800

    System模块增加 GetClipboard 和 SetClipboard方法

--- IsaacSocket-DLL/system_.hpp
+++ IsaacSocket-DLL/system_.hpp
@@ -17,6 +17,81 @@ namespace system_
 		return 0;
 	}
 
+	//获取剪贴板
+	static int GetClipboard(lua_State* L) {
+		lua->lua_pushnil(L);
+
+		if (!OpenClipboard(nullptr)) {
+			//无法打开剪贴板
+			return 1;
+		}
+
+		HANDLE hClipboardData = GetClipboardData(CF_UNICODETEXT);
+		if (!hClipboardData) {
+			//无法获取剪贴板数据
+			CloseClipboard();
+			return 1;
+		}
+
+		wchar_t* u16 = (wchar_t*)(GlobalLock(hClipboardData));
+		if (u16 == nullptr) {
+			//无法锁定剪贴板
+			CloseClipboard();
+			return 1;
+		}
+
+		size_t len = utils::U16ToU8(u16);
+		char* u8 = new char[len];
+		utils::U16ToU8(u16, u8, len);
+
+		GlobalUnlock(hClipboardData);
+		CloseClipboard();
+
+		lua->lua_pushstring(L, u8);
+
+		delete[] u8;
+
+		return 1;
+	}
+
+	static int SetClipboard(lua_State* L) {
+		if (!OpenClipboard(nullptr)) {
+			//无法打开剪贴板
+			return 0;
+		}
+
+		EmptyClipboard();
+		ARG_DEF(1, string, const char*, text, "");
+		size_t size = utils::U8ToU16(text);
+		HGLOBAL hClipboardData = GlobalAlloc(GMEM_MOVEABLE, size * sizeof(wchar_t));
+		if (!hClipboardData) {
+			//无法分配内存
+			CloseClipboard();
+			return 0;
+		}
+
+		wchar_t* u16 = (wchar_t*)(GlobalLock(hClipboardData));
+
+		if (!u16) {
+			//无法锁定内存
+			GlobalFree(hClipboardData);
+			CloseClipboard();
+			return 0;
+		}
+
+		utils::U8ToU16(text, u16, size);
+
+		GlobalUnlock(hClipboardData);
+
+		// 将数据放入剪贴板
+		SetClipboardData(CF_UNICODETEXT, hClipboardData);
+
+		// 关闭剪贴板
+		CloseClipboard();
+
+		return 0;
+	}
+
 	void Init(lua_State* L, lua::Lua* lua) {
 		size_t top = lua->lua_gettop(L);
 		system_::lua = lua;
@@ -30,6 +105,8 @@ namespace system_
 #define _(name) lua->lua_pushstring(L, #name);lua->lua_pushcfunction(L, name); lua->lua_settable(L, -3)
 
 		_(ConsoleOutput);
+		_(GetClipboard);
+		_(SetClipboard);
 
 #undef _
 
