4ef00ca6c3acd8ab6ed13f11747625573679b756
LanbingIce <895446745@qq.com>
2024-01-30 16:58:28 +0800

    WinAPI模块增加方法MessageBox

--- IsaacSocket-DLL/win_api.hpp
+++ IsaacSocket-DLL/win_api.hpp
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "module.hpp"
+#undef MessageBox
 
 namespace win_api
 {
@@ -24,12 +25,30 @@ namespace win_api
 		RET(integer, ::GetKeyState(nVirtKey));
 	}
 
+	static int MessageBox(lua_State* L) {
+		ARG(1, integer, HWND, hWnd);
+		ARG(2, string, LPCSTR, lpText);
+		ARG(3, string, LPCSTR, lpCaption);
+		ARG(4, integer, UINT, uType);
+
+		size_t len_t = utils::U8ToU16(lpText);
+		vector<wchar_t> u16_t(len_t);
+		utils::U8ToU16(lpText, u16_t.data(), len_t);
+
+		size_t len_c = utils::U8ToU16(lpCaption);
+		vector<wchar_t> u16_c(len_c);
+		utils::U8ToU16(lpCaption, u16_c.data(), len_c);
+
+		RET(integer, MessageBoxW(hWnd, u16_t.data(), u16_c.data(), uType));
+	}
+
 	static void Init() {
 		DEFMOD(WinAPI);
 		DEF(AllocConsole);
 		DEF(FreeConsole);
 		DEF(GetAsyncKeyState);
 		DEF(GetKeyState);
+		DEF(MessageBox);
 		ENDMOD();
 	}
 }
