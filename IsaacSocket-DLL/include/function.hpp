5f730070debe1d4e192952d86231f4c41b71c9bb
LanbingIce <895446745@qq.com>
2024-02-11 01:25:41 +0800

    设置ImGui的默认字体为微软雅黑

--- IsaacSocket-DLL/include/function.hpp
+++ IsaacSocket-DLL/include/function.hpp
@@ -70,6 +70,7 @@ namespace function {
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;   // Enable Keyboard Controls
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;    // Enable Gamepad Controls
+		ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\msyh.ttc", 18.0f, nullptr, io.Fonts->GetGlyphRangesChineseFull());
 
 		// Setup Dear ImGui style
 		ImGui::StyleColorsDark();
