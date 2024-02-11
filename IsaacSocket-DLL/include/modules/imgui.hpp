3c768dbf37adc8080933d69122364fb4e6c43e66
LanbingIce <895446745@qq.com>
2024-02-11 15:50:08 +0800

    ImGui模块增加方法SliderFloat SliderInt InputInt SameLine

--- IsaacSocket-DLL/include/modules/imgui.hpp
+++ IsaacSocket-DLL/include/modules/imgui.hpp
@@ -40,6 +40,52 @@ namespace imgui {
 		return 2;
 	}
 
+	static int SliderFloat(lua_State* L) {
+		ARG(1, string, const char*, label);
+		ARG(2, number, float, v);
+		ARG(3, number, float, v_min);
+		ARG(4, number, float, v_max);
+		ARG_DEF(5, string, const char*, format, "%.3f");
+		ARG_DEF(6, integer, int, flags, 0);
+
+		local.lua.lua_pushboolean(L, ImGui::SliderFloat(label, &v, v_min, v_max, format, flags));
+		local.lua.lua_pushnumber(L, v);
+		return 2;
+	}
+
+	static int SliderInt(lua_State* L) {
+		ARG(1, string, const char*, label);
+		ARG(2, integer, int, v);
+		ARG(3, number, float, v_min);
+		ARG(4, number, float, v_max);
+		ARG_DEF(5, string, const char*, format, "%d");
+		ARG_DEF(6, integer, int, flags, 0);
+
+		local.lua.lua_pushboolean(L, ImGui::SliderInt(label, &v, v_min, v_max, format, flags));
+		local.lua.lua_pushinteger(L, v);
+		return 2;
+	}
+
+	static int InputInt(lua_State* L) {
+		ARG(1, string, const char*, label);
+		ARG(2, integer, int, v);
+		ARG_DEF(3, integer, int, step, 1);
+		ARG_DEF(4, integer, int, step_fast, 100);
+		ARG_DEF(5, integer, ImGuiInputTextFlags, flags, 0);
+
+		local.lua.lua_pushboolean(L, ImGui::InputInt(label, &v, step_fast, flags));
+		local.lua.lua_pushinteger(L, v);
+		return 2;
+	}
+
+	static int SameLine(lua_State* L) {
+		ARG_DEF(1, number, float, offset_from_start_x, 0.0F);
+		ARG_DEF(2, number, float, spacing_w, -1.0F);
+
+		ImGui::SameLine(offset_from_start_x, spacing_w);
+		return 0;
+	}
+
 	static void Init() {
 		DEFMOD(ImGui);
 		DEF(Begin);
@@ -47,6 +93,10 @@ namespace imgui {
 		DEF(Button);
 		DEF(Text);
 		DEF(Checkbox);
+		DEF(SliderFloat);
+		DEF(SliderInt);
+		DEF(InputInt);
+		DEF(SameLine);
 		ENDMOD();
 	}
 }
