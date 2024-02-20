0b1be5f7df9fdd74b90400ef5f34bafbd4a22c13
LanbingIce <895446745@qq.com>
2024-02-20 14:10:39 +0800

    修改System模块剪贴板方法的实现，改为使用ImGui的实现

--- IsaacSocket-DLL/include/modules/system_.hpp
+++ IsaacSocket-DLL/include/modules/system_.hpp
@@ -15,76 +15,15 @@ namespace system_
 		return 0;
 	}
 
-	//获取剪贴板
+	// 获取剪贴板
 	static int GetClipboard(lua_State* L) {
-		local.lua.lua_pushnil(L);
-
-		if (!OpenClipboard(nullptr)) {
-			//无法打开剪贴板
-			return 1;
-		}
-
-		HANDLE hClipboardData = GetClipboardData(CF_UNICODETEXT);
-		if (!hClipboardData) {
-			//无法获取剪贴板数据
-			CloseClipboard();
-			return 1;
-		}
-
-		wchar_t* u16 = (wchar_t*)(GlobalLock(hClipboardData));
-		if (u16 == nullptr) {
-			//无法锁定剪贴板
-			CloseClipboard();
-			return 1;
-		}
-
-		size_t len = utils::U16ToU8(u16);
-		vector<char> u8(len);
-		utils::U16ToU8(u16, u8.data(), len);
-
-		GlobalUnlock(hClipboardData);
-		CloseClipboard();
-
-		local.lua.lua_pushstring(L, u8.data());
-
-		return 1;
+		RET(string, ImGui::GetClipboardText());
 	}
 
+	// 设置剪贴板
 	static int SetClipboard(lua_State* L) {
-		if (!OpenClipboard(nullptr)) {
-			//无法打开剪贴板
-			return 0;
-		}
-
-		EmptyClipboard();
 		ARG_DEF(1, string, const char*, text, "");
-		size_t size = utils::U8ToU16(text);
-		HGLOBAL hClipboardData = GlobalAlloc(GMEM_MOVEABLE, size * sizeof(wchar_t));
-		if (!hClipboardData) {
-			//无法分配内存
-			CloseClipboard();
-			return 0;
-		}
-
-		wchar_t* u16 = (wchar_t*)(GlobalLock(hClipboardData));
-
-		if (!u16) {
-			//无法锁定内存
-			GlobalFree(hClipboardData);
-			CloseClipboard();
-			return 0;
-		}
-
-		utils::U8ToU16(text, u16, size);
-
-		GlobalUnlock(hClipboardData);
-
-		// 将数据放入剪贴板
-		SetClipboardData(CF_UNICODETEXT, hClipboardData);
-
-		// 关闭剪贴板
-		CloseClipboard();
-
+		ImGui::SetClipboardText(text);
 		return 0;
 	}
 
