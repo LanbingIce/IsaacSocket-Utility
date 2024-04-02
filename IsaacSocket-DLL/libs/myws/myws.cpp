8f5adbaf9fbdc30b187fa2a1da148b9a1948e3f8
LanbingIce <895446745@qq.com>
2024-04-02 16:59:06 +0800

    封装一个ws库

---
+++ IsaacSocket-DLL/libs/myws/myws.cpp
@@ -0,0 +1,121 @@
+﻿#include "myws.hpp"
+
+#include <Poco/URI.h>
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPMessage.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Buffer.h>
+#include <Poco/Task.h>
+#include <Poco/TaskManager.h>
+#include <Poco/ThreadPool.h>
+
+namespace myws {
+    class _Task :public Poco::Task
+    {
+    public:
+        _Task(std::function<void()> callback) : Task(""), _callback(callback) {}
+        void runTask() {
+            _callback();
+        }
+    private:
+        std::function <void()> _callback;
+    };
+
+    void MyWS::_Connect(const string& url) {
+        try
+        {
+            Poco::URI uri(url);
+            Poco::Net::HTTPClientSession session(uri.getHost(), uri.getPort());
+            Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_GET, "/?encoding=text", Poco::Net::HTTPMessage::HTTP_1_1);
+            request.set("origin", "http://www.websocket.org");
+            Poco::Net::HTTPResponse response;
+            _pws = std::make_shared<Poco::Net::WebSocket>(session, request, response);
+            _pws->setReceiveTimeout(0);
+            Poco::Buffer<char> _buffer(0);
+            SetState(OPEN);
+            OnOpen();
+            while (true)
+            {
+                _buffer.resize(0);
+                int flags = 0;
+                int len = _pws->receiveFrame(_buffer, flags);
+                const char* buffer = _buffer.begin();
+                if (flags & _pws->FRAME_OP_CLOSE)
+                {
+                    SetState(CLOSING);
+                    short closeStatus = 0;
+                    char* p = (char*)&closeStatus;
+                    //反转字节序
+                    p[0] = buffer[1];
+                    p[1] = buffer[0];
+                    _pws->sendFrame(buffer, len, flags);
+                    SetState(CLOSED);
+                    OnClose(closeStatus, string(buffer + 2, len - 2));
+                    break;
+                }
+                else
+                {
+                    OnMessage(buffer, len, flags);
+                }
+            }
+        }
+        catch (Poco::Exception& e)
+        {
+            SetState(CLOSED);
+            OnError(e.displayText());
+        }
+        catch (const std::exception& e)
+        {
+            SetState(CLOSED);
+            OnError(e.what());
+        }
+    }
+
+    int MyWS::Send(const char* message, int len, bool isBinary) {
+        if (GetState() != OPEN)
+        {
+            return -1;
+        }
+        return  _pws->sendFrame(message, len, isBinary ? Poco::Net::WebSocket::FRAME_BINARY : Poco::Net::WebSocket::FRAME_TEXT);
+    }
+
+    bool MyWS::Close(short closeStatus, const string& statusDescription) {
+        if (GetState() != OPEN)
+        {
+            return false;
+        }
+        SetState(CLOSING);
+
+        _pws->shutdown(closeStatus, statusDescription);
+
+        while (true)
+        {
+            Sleep(1);
+            if (GetState() != CLOSING)
+            {
+                break;
+            }
+        }
+        return true;
+    }
+
+    WebSocketState MyWS::GetState() {
+        std::lock_guard lock(_mutex);
+        return _state;
+    }
+
+    void MyWS::SetState(WebSocketState state) {
+        std::lock_guard lock(_mutex);
+        _state = state;
+    }
+
+    MyWS::MyWS(const string& url) {
+        static Poco::ThreadPool pool(2, 256);
+        static Poco::TaskManager taskManager(pool);
+        taskManager.start(new _Task([this, url] {
+            _Connect(url);
+            }));
+    }
+}
+#undef LOCK_GUARD
---
+++ IsaacSocket-DLL/libs/myws/myws.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <Poco/Net/WebSocket.h>
+
+namespace myws {
+    enum WebSocketState
+    {
+        CONNECTING,
+        OPEN,
+        CLOSING,
+        CLOSED
+    };
+
+    class MyWS
+    {
+    public:
+        std::function<void()> OnOpen = [] {};
+        std::function<void(const char* message, int len, int flags)> OnMessage = [](const char*, int, int) {};
+        std::function<void(short closeStatus, const string& statusDescription)> OnClose = [](short, const string&) {};
+        std::function<void(const string& error)> OnError = [](const string&) {};
+
+        int Send(const char* message, int len, bool isBinary = false);
+        bool Close(short closeStatus = 1000, const string& statusDescription = "");
+
+        void SetState(WebSocketState state);
+        WebSocketState GetState();
+
+        MyWS(const string& url);
+    private:
+        void _Connect(const string& url);
+        std::shared_ptr<Poco::Net::WebSocket> _pws;
+        std::mutex _mutex;
+        WebSocketState _state = CONNECTING;
+    };
+}
