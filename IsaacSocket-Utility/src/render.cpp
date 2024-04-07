d49cf852f6567625bfffb67e6c0987f559ab1413
LanbingIce <895446745@qq.com>
2024-04-07 23:57:12 +0800

    将渲染逻辑提取到单独文件

---
+++ IsaacSocket-Utility/src/render.cpp
@@ -0,0 +1,12 @@
+﻿#include <render.hpp>
+#include <imgui/imgui.h>
+namespace render {
+    void Render() {
+        ImGuiWindowFlags flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize;
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+        ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+        ImGui::Begin("##IsaacSocket_Window", nullptr, flags);
+        ImGui::Button("button");
+        ImGui::End();
+    }
+}
