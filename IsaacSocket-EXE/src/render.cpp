6632fc5c2f1dabe62afd7f4d51ab38141cc7055d
LanbingIce <895446745@qq.com>
2024-04-09 00:16:10 +0800

    exe:控制台逻辑

--- IsaacSocket-EXE/src/render.cpp
+++ IsaacSocket-EXE/src/render.cpp
@@ -5,14 +5,31 @@ namespace render_ {
     bool show_demo_window = true;
     bool show_another_window = false;
     bool Render() {
-        bool open = true; 
-        
+        bool open = true;
+        bool console = GetConsoleWindow();
+
         ImGui::Begin("##IsaacSocket_Window", &open);
+        if (ImGui::Checkbox("Console", &console))
+        {
+            if (console)
+            {
+                AllocConsole();
+                SetConsoleOutputCP(CP_UTF8);
+                FILE* stream;
+                freopen_s(&stream, "CONOUT$", "w", stdout);
+            }
+            else
+            {
+                ShowWindow(GetConsoleWindow(), SW_HIDE);
+                FreeConsole();
+            }
+        }
+
         if (ImGui::Button("button"))
         {
-            mb("hello");
+            cout << "测试";
         }
-        
+
         ImGui::End();
         return !open;
     }
